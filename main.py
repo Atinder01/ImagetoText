6662772097304eb685c2978c2298574545a2fc6b
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-09-09 10:36:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.title("Get text from image")
 image = Image.open("image2text.png")
 st.image(image, use_column_width=True)
 
-pytesseract.pytesseract.tesseract_cmd = "Tesseract-OCR\\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = "tesseract.exe"
 st.write("""
 ** Convert Image To Text **
 """)
