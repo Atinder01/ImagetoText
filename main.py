5737df6ae777c8e093654de86ae06bb26400e086
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-09-09 11:37:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if uploaded_file is not None:
             #   pdf.cell(200,10," ")
     st.image(img, caption='Text in your image.', use_column_width=True, clamp=True)
     credits("Text in your image goes here: ")
-    st.write(f'<p style="background-color:#0796B6;">{t}</p>', unsafe_allow_html=True)
+    st.markdown(f'<p style="background-color:#0796B6;">{t}</p>', unsafe_allow_html=True)
 
 st.text("")
 st.text("")
