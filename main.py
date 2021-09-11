fcaf6c61bce0b94064f82917cd78cd98a59c88ba
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-09-11 16:30:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import cv2
 from PIL import Image
 import numpy as np
 
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
 def credits(content):
     st.markdown(
         f'<p style="color:{"#0796B6"};">{content}</p>',
@@ -24,6 +27,7 @@ if file is not None:
   try:
     file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, 1)
+    img=remove_noise(img)
     hImg,wImg,_=img.shape
     boxes = pytesseract.image_to_data(img)
     t = pytesseract.image_to_string(img)
