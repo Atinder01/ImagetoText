47769a0a4aa1c55a305e820ad09e11d1721f3fc3
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-09-11 16:28:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pytesseract
 import cv2
 from PIL import Image
 import numpy as np
-import filetype
 
 def credits(content):
     st.markdown(
@@ -20,9 +19,8 @@ credits("""
 If you want to find out how to turn an image into a text document, you came to the right place. This free online tool allows you to convert from image to text.
 Upload the image from which you need to extract text
 """)
-uploaded_file = st.file_uploader("")
-file=uploaded_file
-if uploaded_file is not None:
+file = st.file_uploader("")
+if file is not None:
   try:
     file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, 1)
@@ -36,10 +34,6 @@ if uploaded_file is not None:
             x,y,w,h=int(b[6]),int(b[7]),int(b[8]),int(b[9])
             cv2.rectangle(img,(x,y),(w+x,h+y),(50,50,50),1)
             cv2.putText(img,b[11],(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,50),1)
-            #try:
-            #    pdf.cell(200, 10, b[11])
-            #except UnicodeEncodeError:
-            #   pdf.cell(200,10," ")
     st.image(img, caption='Text in your image.', use_column_width=True, clamp=True)
     credits("Text in your image goes here: ")
     st.write(t)
